d2e719615f95f0aa054c013039e0a02c9df8349c
Walmyr <wlsf82@gmail.com>
2022-01-05 23:55:35 +0100

    Create and use login custom cmd

--- cypress/integration/sauceDemo.spec.js
+++ cypress/integration/sauceDemo.spec.js
@@ -2,12 +2,7 @@ describe('SWAG Labs', () => {
   beforeEach(() => cy.visit('/'))
 
   it('logs in with standard user', () => {
-    cy.dataTest('username')
-      .type(Cypress.env('user'))
-    cy.dataTest('password')
-      .type(Cypress.env('password'), { log: false })
-    cy.dataTest('login-button')
-      .click()
+    cy.login()
 
     cy.url()
       .should('be.equal', `${Cypress.config('baseUrl')}/inventory.html`)
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,15 @@
 Cypress.Commands.add('dataTest', value => {
   return cy.get(`[data-test="${value}"]`)
 })
+
+Cypress.Commands.add('login', (
+  user = Cypress.env('user'),
+  password = Cypress.env('password')
+) => {
+  cy.dataTest('username')
+    .type(user)
+  cy.dataTest('password')
+    .type(password, { log: false })
+  cy.dataTest('login-button')
+    .click()
+})
