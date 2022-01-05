7add9ae59ff649b8b7db07aeca8ff245e5d0c056
Walmyr <wlsf82@gmail.com>
2022-01-05 23:57:06 +0100

    Create new tests and a validate login custom cmd

--- cypress/integration/sauceDemo.spec.js
+++ cypress/integration/sauceDemo.spec.js
@@ -4,9 +4,27 @@ describe('SWAG Labs', () => {
   it('logs in with standard user', () => {
     cy.login()
 
-    cy.url()
-      .should('be.equal', `${Cypress.config('baseUrl')}/inventory.html`)
-    cy.contains('.title', 'Products')
-      .should('be.visible')
+    cy.validateSucessfullLogin()
   })
+
+  it('error on locked user', () => {
+    cy.login('locked_out_user', Cypress.env('password'))
+
+    cy.dataTest('error')
+      .should('be.visible')
+      .and('contain', 'Sorry, this user has been locked out.')
+  })
+
+  it('logs in with performance glitch user', () => {
+    cy.login('performance_glitch_user', Cypress.env('password'))
+
+    cy.validateSucessfullLogin()
+  })
+})
+
+Cypress.Commands.add('validateSucessfullLogin', () => {
+  cy.url()
+    .should('be.equal', `${Cypress.config('baseUrl')}/inventory.html`)
+  cy.contains('.title', 'Products')
+    .should('be.visible')
 })
